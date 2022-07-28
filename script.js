e7d2ae8c426aebb972bd1d3747a8afb96ff1a686
NicolasBOUDART <109206824+NicolasBOUDART@users.noreply.github.com>
2022-07-28 15:40:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+function startingblock () {
+    var music = document.getElementById("ost");
+    music.volume = 0.2;
+    music.play()
+    document.getElementById('nstart').id="nupleft"
+    document.getElementById('buttonstart').id="baccueil"
+    document.getElementById('menu').id="menu1"
+    document.getElementById('baccueil').onclick="none"
+}
+function affichemorpion() {
+    var a = document.getElementById("morpion")
+    if ( a.id==="morpion"){
+        a.id="morpionvisible"
+        }
+        document.getElementById('player1').id="player1visible"
+        document.getElementById('player2').id="player2visible"
+    }
+function changecase1player1() {
+    var a = document.getElementById("bcase1")
+    if (a.src!="ress/O.png" || a.src==="" ) {
+        a.src="ress/X.png"
+    }
+}
+function changecase1player2() {
+    var a = document.getElementById("bcase2")
+    if (a.src!="ress/O.png" || a.src==="" ) {
+        a.src="ress/X.png"
+    }
+}
+function changecase1player3() {
+    var a = document.getElementById("bcase3")
+    if (a.src!="ress/O.png" || a.src==="" ) {
+        a.src="ress/X.png"
+    }
+}
+function changecase1player4() {
+    var a = document.getElementById("bcase4")
+    if (a.src!="ress/O.png" || a.src==="" ) {
+        a.src="ress/X.png"
+    }
+}
+function changecase1player5() {
+    var a = document.getElementById("bcase5")
+    if (a.src!="ress/O.png" || a.src==="" ) {
+        a.src="ress/X.png"
+    }
+}
+function changecase1player6() {
+    var a = document.getElementById("bcase6")
+    if (a.src!="ress/O.png" || a.src==="" ) {
+        a.src="ress/X.png"
+    }
+}
+function changecase1player7() {
+    var a = document.getElementById("bcase7")
+    if (a.src!="ress/O.png" || a.src==="" ) {
+        a.src="ress/X.png"
+    }
+}
+function changecase1player8() {
+    var a = document.getElementById("bcase8")
+    if (a.src!="ress/O.png" || a.src==="" ) {
+        a.src="ress/X.png"
+    }
+}
+function changecase1player9() {
+    var a = document.getElementById("bcase9")
+    if (a.src!="ress/O.png" || a.src==="" ) {
+        a.src="ress/X.png"
+    }
+}
+function changecase2player1() {
+    var a = document.getElementById("bcase1")
+    if (a.src!="ress/X.png" || a.src==="" ) {
+        a.src="ress/O.png"
+    }
+}
+function changecase2player2() {
+    var a = document.getElementById("bcase2")
+    if (a.src!="ress/X.png" || a.src==="" ) {
+        a.src="ress/O.png"
+    }
+}
+function changecase2player3() {
+    var a = document.getElementById("bcase3")
+    if (a.src!="ress/X.png" || a.src==="" ) {
+        a.src="ress/O.png"
+    }
+}
+function changecase2player4() {
+    var a = document.getElementById("bcase4")
+    if (a.src!="ress/X.png" || a.src==="" ) {
+        a.src="ress/O.png"
+    }
+}
+function changecase2player5() {
+    var a = document.getElementById("bcase5")
+    if (a.src!="ress/X.png" || a.src==="" ) {
+        a.src="ress/O.png"
+    }
+}
+function changecase2player6() {
+    var a = document.getElementById("bcase6")
+    if (a.src!="ress/X.png" || a.src==="" ) {
+        a.src="ress/O.png"
+    }
+}
+function changecase2player7() {
+    var a = document.getElementById("bcase7")
+    if (a.src!="ress/X.png" || a.src==="" ) {
+        a.src="ress/O.png"
+    }
+}
+function changecase2player8() {
+    var a = document.getElementById("bcase8")
+    if (a.src!="ress/X.png" || a.src==="" ) {
+        a.src="ress/O.png"
+    }
+}
+function changecase2player9() {
+    var a = document.getElementById("bcase9")
+    if (a.src!="ress/X.png" || a.src==="" ) {
+        a.src="ress/O.png"
+    }
+}
+
